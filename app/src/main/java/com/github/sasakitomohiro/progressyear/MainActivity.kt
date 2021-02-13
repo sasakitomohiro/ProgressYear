8331c5c37ec7419d404547e8ff3d4e1eab604fb3
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2021-02-13 23:30:46 +0900

    add progress bar

--- app/src/main/java/com/github/sasakitomohiro/progressyear/MainActivity.kt
+++ app/src/main/java/com/github/sasakitomohiro/progressyear/MainActivity.kt
@@ -2,10 +2,23 @@ package com.github.sasakitomohiro.progressyear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.github.sasakitomohiro.progressyear.databinding.ActivityMainBinding
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val now = LocalDate.now()
+        val dayOfYear = now.dayOfYear.toFloat()
+        val yearDays = LocalDate.of(now.year, 12, 31).dayOfYear.toFloat()
+        val progressOfYear = dayOfYear / yearDays
+        binding.yearDays.text = (progressOfYear * 100).toString()
+        binding.progressBar.setProgressWithAnimation(progressOfYear * 100)
     }
 }
