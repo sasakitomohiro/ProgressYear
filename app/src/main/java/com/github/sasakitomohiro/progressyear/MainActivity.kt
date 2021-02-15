ef25ec8486de9609a2889fdc96f9259e7c2a4121
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2021-02-15 21:17:35 +0900

    modify layout

--- app/src/main/java/com/github/sasakitomohiro/progressyear/MainActivity.kt
+++ app/src/main/java/com/github/sasakitomohiro/progressyear/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.sasakitomohiro.progressyear
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -11,14 +12,15 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val now = LocalDate.now()
         val dayOfYear = now.dayOfYear.toFloat()
         val yearDays = LocalDate.of(now.year, 12, 31).dayOfYear.toFloat()
-        val progressOfYear = dayOfYear / yearDays
-        binding.yearDays.text = (progressOfYear * 100).toString()
-        binding.progressBar.setProgressWithAnimation(progressOfYear * 100)
+        val progressOfYear = (dayOfYear / yearDays) * 100
+        binding.yearDays.text = "%.2f".format(progressOfYear)
+        binding.progressBar.setProgressWithAnimation(progressOfYear)
     }
 }
